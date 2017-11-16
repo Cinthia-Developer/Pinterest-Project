e24bf81d81833bedd998a71909658df3e1e13741
Cinthia Mashiel <cinthiamashiel474@gmail.com>
2017-11-16 15:15:14 -0500

    Se logró subir las imágenes.

--- js/plugin.js
+++ js/plugin.js
@@ -1,2 +1,13 @@
-
-
+$.fn.pinterest = function(listaImage, title){
+    if(listaImage.length !==0){
+        for(var i=0; i<listaImage.length; i++){
+            let contImage =  '<img class="contImage" alt="">';
+            let url = listaImage[i];
+            let $cont_image = $(contImage);
+            $cont_image.attr("src", url);
+            $cont_image.css("width", "20%");
+            $cont_image.css("padding", "5px");
+            $(this).append($cont_image);
+        };
+    }
+}
