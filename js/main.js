d9a5c8b49e929509523ba3e299e7fda532c65e98
Cinthia Mashiel <cinthiamashiel474@gmail.com>
2017-11-16 16:42:56 -0500

    Se subió el ultimo cambio.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,16 @@
 $(document).ready(function(){                             
     $(".elemento").pinterest([
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", 
-        "https://lh3.googleusercontent.com/mY3TYW7nUnequ9P1Y8VHyc_2FKsK9EfFr4FvzhPDasI5FbX-vg9P-HV9YoOyM_7JyKs=h900",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGqfPWvuyrx0wj9myyCvY_yllkR7BuVAMYxd7EUPoYJpmHoysf",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlqkI5kX611Jg65kGOAt7wcoVQ7lLxYGrMuRccmhKfCN1lChIe", 
-        "http://imagenesdeamorbellas.net/wp-content/uploads/2016/07/9-10.gif"
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA",
+        "hola"
     ]);
     $(".elemento-2").pinterest([
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", 
-        "https://lh3.googleusercontent.com/mY3TYW7nUnequ9P1Y8VHyc_2FKsK9EfFr4FvzhPDasI5FbX-vg9P-HV9YoOyM_7JyKs=h900",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGqfPWvuyrx0wj9myyCvY_yllkR7BuVAMYxd7EUPoYJpmHoysf",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlqkI5kX611Jg65kGOAt7wcoVQ7lLxYGrMuRccmhKfCN1lChIe", 
-        "http://imagenesdeamorbellas.net/wp-content/uploads/2016/07/9-10.gif"
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA",
+        "hola"
     ]);
     $(".elemento-3").pinterest([
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", 
-        "https://lh3.googleusercontent.com/mY3TYW7nUnequ9P1Y8VHyc_2FKsK9EfFr4FvzhPDasI5FbX-vg9P-HV9YoOyM_7JyKs=h900",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGqfPWvuyrx0wj9myyCvY_yllkR7BuVAMYxd7EUPoYJpmHoysf",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlqkI5kX611Jg65kGOAt7wcoVQ7lLxYGrMuRccmhKfCN1lChIe", 
-        "http://imagenesdeamorbellas.net/wp-content/uploads/2016/07/9-10.gif"
-    ],
-    [
-      "hola"  
-    ]);
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA",
+        "hola"
+    ])
 });
 
 
--- js/plugin.js
+++ js/plugin.js
@@ -1,11 +1,12 @@
-$.fn.pinterest = function(listaImage, title){
+$.fn.pinterest = function(listaImage){
+    
     if(listaImage.length !==0){
         for(var i=0; i<listaImage.length; i++){
-            let contImage =  '<img class="contImage" alt="">';
+            let contImage =  '<img class="contImage" alt=""><br>';
             let url = listaImage[i];
             let $cont_image = $(contImage);
             $cont_image.attr("src", url);
-            $cont_image.css("width", "20%");
+            $cont_image.css("width", "100%");
             $cont_image.css("padding", "5px");
             $(this).append($cont_image);
         };
