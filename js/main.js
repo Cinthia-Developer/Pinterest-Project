352c9726f0fb022cef1fa391702daabf45174a4a
Cinthia Mashiel <cinthiamashiel474@gmail.com>
2017-11-17 16:25:49 -0500

    Todo el Contenido está Perfecto.

--- js/main.js
+++ js/main.js
@@ -1,51 +1,50 @@
 $(document).ready(function(){                             
     $(".elemento").pinterest(
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria",
-        "https://i.ytimg.com/vi/gg7odj4FpVE/maxresdefault.jpg", "Sebastian"
+        "https://cdn.tn.com.ar/sites/default/files/styles/1366x765/public/2015/08/08/gatito.jpg", "Gatito Bebé", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, pariatur optio illo nobis!",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqMSZmjI0cUoSLXZI_q448IzjgIQSTcRn_jiB9cr2cgc_nfO9kvQ", "Sebastian Andrade"
     );
     $(".elemento").pinterest(
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+        "https://ae01.alicdn.com/kf/HTB1WiKFQpXXXXX.aXXXq6xXFXXX1/Winter-Womens-woolen-coats-sobretudo-feminino-Slim-long-wool-Navy-coat-fall-jacket-casacos-de-la.jpg_640x640.jpg", "Moda Invierno", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, pariatur optio illo nobis!","https://st-listas.20minutos.es/images/2010-02/196194/list_640px.jpg?1274017725", "Natalia Vega"
     );
     $(".elemento").pinterest(
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+        "http://trotamundeando.com/wp-content/uploads/2015/05/Razones-para-visitar-el-Caribe-en-crucero-3.jpg", "Visita el Karibe", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, pariatur optio illo nobis!", "https://pbs.twimg.com/profile_images/570775277895028736/w3n_frip.jpeg", "Ana Zalazar"
     );
     $(".elemento").pinterest(
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+        "https://ae01.alicdn.com/kf/HTB1lScoaTZRMeJjSspnq6AJdFXaq/2017-Women-dress-O-Neck-Three-dimensional-Vertical-Lines-Long-Cotton-Base-Sweater-Dresses-Gray-6319.jpg_220x220.jpg", "Falda Invierno", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, pariatur optio illo nobis!", "https://pbs.twimg.com/profile_images/522145107785428992/zP6X2qUd.jpeg", "Maria Bustinza"
     );
     $(".elemento").pinterest(
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+        "https://cdn0.bodas.com.mx/usr/4/7/2/7/cfb_275132.jpg", "El Amor es:", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, pariatur optio illo nobis!", "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Frank_De_Winne_2009.jpg/1200px-Frank_De_Winne_2009.jpg", "Franco Mendoza"
     );
     $(".elemento").pinterest(
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+        "https://i.pinimg.com/736x/9f/c6/e9/9fc6e9e09e948308b052105fe8bd183b--sites-striped-shirts.jpg", "Moda Casual", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, pariatur optio illo nobis!", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR17R3atwDDh4QwoCHqxL_qZiCb1ymKh4Zy0CwAX3kfzZFdVOA3", "Miriam Mendoza Chavez"
     );
     $(".elemento").pinterest(
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+        "https://media.virtuoso.com/m/cms/VCom/Destination/regions/europe/Finland/Helsinki/Helsinki-Night.jpg", "A Visitar Finlandia", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, pariatur optio illo nobis!", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQD_tCJW7zdziBCa2ZzNbYO0esuGXpfRrlrY1mY8vJloSobuRE64A", "Sofia de Villamore"
     );
     $(".elemento").pinterest(
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+        "https://s-media-cache-ak0.pinimg.com/originals/a6/6a/aa/a66aaaf84fde99ac3fb68776f81dc93e.jpg", "Todo Empezó Así", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, pariatur optio illo nobis!", "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Frank_De_Winne_2009.jpg/1200px-Frank_De_Winne_2009.jpg", "Franco Mendoza"
     );
     $(".elemento").pinterest(
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+        "http://imagenesdetaconesaltos.com/wp-content/uploads/2016/06/zapatos-de-fiesta-para-dama-altos.jpg", "Hermosos Zapatos", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, pariatur optio illo nobis!", "https://yt3.ggpht.com/-cVEo6UUVdxA/AAAAAAAAAAI/AAAAAAAAAAA/ENZk12iNs9o/s900-c-k-no-mo-rj-c0xffffff/photo.jpg", "Yessica Zantander"
     );
     $(".elemento").pinterest(
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5jpoeG27qhJ7Lcn3se9wOpYb9zEBflIYlMoUg9DGICww8xTYrAQ", "La Amistad", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, pariatur optio illo nobis!", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYLZYn3eKd_BtHRlPomgCKYDSFXBEmg3HZBEqoxv90Uam1DNjjow", "Patricia Cortez"
     );
     $(".elemento").pinterest(
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+        "http://www.dimelaneta.com/wp-content/uploads/2013/08/north-island-seychelles-01ch.jpg", "Un día de Relax", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, pariatur optio illo nobis!", "https://pbs.twimg.com/profile_images/785615995880693760/0NIIIqEb.jpg", "Sergio Rodriguez"
     );
     $(".elemento").pinterest(
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+        "http://nutritionedgehd.com/wp-content/uploads/2014/07/assos-2-blazer-3.jpg", "Joven Formal", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, pariatur optio illo nobis!", "https://s-media-cache-ak0.pinimg.com/564x/03/b3/31/03b3314f16c77d9c1f6aa67ac641124d.jpg", "Elisvan Junior TLV"
     );
     $(".elemento").pinterest(
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJW3u0_WKcQXnWkHOgvdU7n1_srn6gZk8aAwVmf0asVGSBVHf5", "Paz y Amor en el Mundo", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, pariatur optio illo nobis!", "https://coo.tuvotacion.com/imagenes_unicas/cual-de-estos-chicos-tiene-el-rostro-mas-bello-1169225.jpg", "Arturo Gonzales"
     );
     $(".elemento").pinterest(
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+        "http://floragalaxy.com/wp-content/uploads/2017/09/2fda7ddca2a2bc90105fcb6d42977d7b.jpg", "Lindo Arreglo Floral", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, pariatur optio illo nobis!", "https://st-listas.20minutos.es/images/2010-02/196194/list_640px.jpg?1274017725", "Natalia Vega"
     );
     $(".elemento").pinterest(
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB-8CwObkleFo1sQlSR_E-9Rdh5agxyLJVcEodYCYzRFfS26rh", "San Valentín", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, pariatur optio illo nobis!", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeYzA2wAaMdA4DIFQJH9btNUlE-D3lCm18CTVdDneFgE3NV5bk", "Mirelia Soza"
     );
-    
 });
 
 
