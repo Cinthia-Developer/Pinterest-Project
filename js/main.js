2b14fe777563c00027c670a7e31ae9fcf0a49c3e
Cinthia Mashiel <cinthiamashiel474@gmail.com>
2017-11-17 12:57:40 -0500

    Se logró hacer un plugin que cree pin, igual a los pin de pinterest.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,50 @@
 $(document).ready(function(){                             
-    $(".elemento").pinterest([
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA",
-        "hola"
-    ]);
-    $(".elemento-2").pinterest([
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA",
-        "hola"
-    ]);
-    $(".elemento-3").pinterest([
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA",
-        "hola"
-    ])
+    $(".elemento").pinterest(
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+    );
+    $(".elemento").pinterest(
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+    );
+    $(".elemento").pinterest(
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+    );
+    $(".elemento").pinterest(
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+    );
+    $(".elemento").pinterest(
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+    );
+    $(".elemento").pinterest(
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+    );
+    $(".elemento").pinterest(
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+    );
+    $(".elemento").pinterest(
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+    );
+    $(".elemento").pinterest(
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+    );
+    $(".elemento").pinterest(
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+    );
+    $(".elemento").pinterest(
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+    );
+    $(".elemento").pinterest(
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+    );
+    $(".elemento").pinterest(
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+    );
+    $(".elemento").pinterest(
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+    );
+    $(".elemento").pinterest(
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvcyYIvXIsGh3VFeXJtssASAK4EoW0adr-b53DVILcNR27YjQGSA", "hola", "loren impsumm master para siempre peru jajaaj te mo peru con p de patria"
+    );
+    
 });
 
 
--- js/plugin.js
+++ js/plugin.js
@@ -1,14 +1,10 @@
-$.fn.pinterest = function(listaImage){
-    
-    if(listaImage.length !==0){
-        for(var i=0; i<listaImage.length; i++){
-            let contImage =  '<img class="contImage" alt=""><br>';
-            let url = listaImage[i];
-            let $cont_image = $(contImage);
-            $cont_image.attr("src", url);
-            $cont_image.css("width", "100%");
-            $cont_image.css("padding", "5px");
-            $(this).append($cont_image);
-        };
-    }
+$.fn.pinterest = function(image, title, description){
+        var html= 
+                  `<div class="cuadro">` + 
+                  `<img src=" ${image} " class="img-responsive" alt="">` + 
+                  `<p> ${title} </p>` + 
+                  `<span> ${description} </span>` +
+                  `</div><!--cierre de cuadro-->`
+        
+        this.append(html); 
 }
